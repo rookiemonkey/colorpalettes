92b12047e52a2cbc229141b81b6534704437f59a
rookiemonkey <kevinroirigorbasina@pm.me>
2020-07-08 09:47:25 +0800

    added styles for color boxes

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -3,12 +3,18 @@ import React, { Component } from 'react';
 class ColorBox extends Component {
     render() {
 
-        const s = { background: this.props.background }
+        const { background, colorName } = this.props
+        const s = { background }
 
         return (
-            <div className="ColorBox" style={s}>
-                <span>{this.props.colorName}</span>
-                <span>More</span>
+            <div className="ColorBox-container" style={s}>
+                <div className="ColorBox-copy-container">
+                    <div className="ColorBox-content-container">
+                        <span className='ColorBox-name'>{colorName}</span>
+                    </div>
+                    <button className="ColorBox-copy">COPY</button>
+                </div>
+                <span className='ColorBox-more'>MORE</span>
             </div>
         )
     }
