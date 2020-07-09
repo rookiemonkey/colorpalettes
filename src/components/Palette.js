646fac9234ca16d7335b14fb3729e98e981c9ff6
rookiemonkey <kevinroirigorbasina@pm.me>
2020-07-09 15:19:26 +0800

    added snackbar

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
 import shortid from 'shortid';
 import NavBar from './NavBar';
 import ColorBox from './ColorBox';
@@ -6,16 +7,22 @@ import ColorBox from './ColorBox';
 class Palette extends Component {
     constructor(props) {
         super(props)
-        this.state = { level: 500, format: 'hex' }
+        this.state = { level: 500, format: 'hex', changedFormat: false }
     }
 
     changeLevel = newLevel => { this.setState({ ...this.state, level: newLevel }) }
 
-    changeFormat = format => { this.setState({ ...this.state, format: format }) }
+    changeFormat = format => {
+        this.setState({ ...this.state, format: format, changedFormat: true }, () => {
+            setTimeout(() => {
+                this.setState({ ...this.state, changedFormat: false })
+            }, 3000)
+        })
+    }
 
     render() {
 
-        const { level, format } = this.state
+        const { level, format, changedFormat } = this.state
         const b = this.props.palette.colors[level].map(c => {
             return (
                 <ColorBox
@@ -38,6 +45,19 @@ class Palette extends Component {
                 <div className="Palette-colors">
                     {b}
                 </div>
+
+                <Snackbar
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    open={changedFormat}
+                    autoHideDuration={3000}
+                    message={<span id="message-id">Color format changed to {format}</span>}
+                    ContentProps={{
+                        'aria-describedby': 'message-id'
+                    }}
+                />
 
             </div>
         )
