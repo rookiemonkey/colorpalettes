54326d8518fb431c9ccd58ff587835d384498d17
rookiemonkey <kevinroirigorbasina@pm.me>
2020-07-09 15:52:07 +0800

    added routes prototype

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Palette from './components/Palette';
 import seeds from './components/seeds';
 import generatePalette from './helpers/generatePalette';
@@ -6,9 +7,19 @@ import generatePalette from './helpers/generatePalette';
 class App extends Component {
   render() {
     return (
-      <div className="Color-App">
-        <Palette palette={generatePalette(seeds[2])} />
-      </div>
+      <Switch>
+
+        <Route
+          exact path='/'
+          render={() => (<h1>This is Home</h1>)}
+        />
+
+        <Route
+          exact path='/palette/:id'
+          render={() => (<Palette palette={generatePalette(seeds[2])} />)}
+        />
+
+      </Switch>
     );
   }
 }
