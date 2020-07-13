3aab3d6962009df2b1b25c95bb25937ecb62cffe
rookiemonkey <kevinroirigorbasina@pm.me>
2020-07-13 13:40:39 +0800

    added functionalities to the form

--- src/components/PaletteForm.js
+++ src/components/PaletteForm.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ChromePicker } from 'react-color'
 import clsx from 'clsx';
-import { useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -12,105 +11,130 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import SaveIcon from '@material-ui/icons/Save';
 import formStyles from '../styles/form';
+import { withStyles } from '@material-ui/styles';
 
-const PaletteForm = () => {
-    const classes = formStyles();
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
+class PaletteForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+            currentColor: 'white',
+            colorBoxes: []
+        }
+    }
 
-    const handleDrawerOpen = () => {
-        setOpen(true);
+    handleDrawerOpen = () => {
+        this.setState({ open: true });
     };
 
-    const handleDrawerClose = () => {
-        setOpen(false);
+    handleDrawerClose = () => {
+        this.setState({ open: false });
     };
 
-    return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <AppBar
-                position="fixed"
-                className={clsx(classes.appBar, {
-                    [classes.appBarShift]: open,
-                })}
-            >
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        className={clsx(classes.menuButton, open && classes.hide)}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" noWrap>
-                        Persistent drawer
+    handleColorChange = newColor => {
+        this.setState({ ...this.state, currentColor: newColor.hex })
+    }
+
+    handleAddColor = newColor => {
+        const { currentColor, colorBoxes } = this.state
+        const newSet = [...colorBoxes, currentColor]
+        this.setState({ ...this.state, colorBoxes: newSet, currentColor: 'white' })
+    }
+
+    render() {
+        const { classes } = this.props
+        const { open, currentColor } = this.state
+        console.log('after rendering', this.state)
+
+        return (
+            <div className={classes.root} >
+                <CssBaseline />
+                <AppBar
+                    position="fixed"
+                    className={clsx(classes.appBar, {
+                        [classes.appBarShift]: open,
+                    })}
+                >
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={this.handleDrawerOpen}
+                            edge="start"
+                            className={clsx(classes.menuButton, open && classes.hide)}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" noWrap>
+                            Persistent drawer
                     </Typography>
-                </Toolbar>
-            </AppBar>
+                    </Toolbar>
+                </AppBar>
 
-            <Drawer
-                className={classes.drawer}
-                variant="persistent"
-                anchor="left"
-                open={open}
-                classes={{
-                    paper: classes.drawerPaper,
-                }}>
+                <Drawer
+                    className={classes.drawer}
+                    variant="persistent"
+                    anchor="left"
+                    open={open}
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}>
 
-                <div className={classes.drawerHeader}>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                    </IconButton>
-                </div>
+                    <div className={classes.drawerHeader}>
+                        <IconButton onClick={this.handleDrawerClose} style={{ float: 'right' }}>
+                            <ChevronLeftIcon />
+                        </IconButton>
+                    </div>
 
-                <Divider />
+                    <Divider />
 
-                <Typography variant="h4" noWrap>
-                    Design your palette
-                </Typography>
+                    <Typography variant="h4" noWrap>
+                        Design your palette
+                    </Typography>
 
-                <div>
+                    <div>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                        >Clear</Button>
+
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                        >Random</Button>
+                    </div>
+
+                    <ChromePicker
+                        color={currentColor}
+                        onChange={this.handleColorChange}
+                    />
+
                     <Button
+                        style={{ backgroundColor: currentColor }}
+                        onClick={this.handleAddColor}
                         variant="contained"
-                        color="primary"
-                    >Clear</Button>
+                        size="large"
+                        className={classes.button}
+                        id='save-button'
+                        startIcon={<SaveIcon />}
+                    >Save</Button>
 
-                    <Button
-                        variant="contained"
-                        color="secondary"
-                    >Random</Button>
-                </div>
+                </Drawer>
 
-                <ChromePicker
-                />
+                <main
+                    className={clsx(classes.content, {
+                        [classes.contentShift]: open,
+                    })}
+                >
+                    <div className={classes.drawerHeader} />
 
-                <Button
-                    variant="contained"
-                    color="primary"
-                    size="large"
-                    className={classes.button}
-                    startIcon={<SaveIcon />}
-                >Save</Button>
-
-            </Drawer>
-
-            <main
-                className={clsx(classes.content, {
-                    [classes.contentShift]: open,
-                })}
-            >
-                <div className={classes.drawerHeader} />
-
-                {/* dragable boxees goes here */}
-            </main>
-        </div>
-    );
+                    {/* dragable boxees goes here */}
+                </main>
+            </div>
+        );
+    }
 }
 
-export default PaletteForm
+export default withStyles(formStyles, { withTheme: true })(PaletteForm)
